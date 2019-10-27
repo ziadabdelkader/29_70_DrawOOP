e8ee946c5a7ce11fb8d8c47ba6f31be12b7f7c0b
ziadabdelkader <zyad012ziad012@gmail.com>
2019-10-27 16:30:52 +0200

    square

---
+++ src/eg/edu/alexu/csd/oop/draw/ID_30/basicShapes/square.java
@@ -0,0 +1,10 @@
+package eg.edu.alexu.csd.oop.draw.ID_30.basicShapes;
+
+public class square extends rectangle{
+
+	public square(int x, int y, double length) {
+		super(x, y, length, length);
+		// TODO Auto-generated constructor stub
+	}
+
+}
